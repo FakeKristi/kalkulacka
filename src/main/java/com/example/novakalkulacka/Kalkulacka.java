093d5e49d59f7a342cdfdf0492728e0c9210ce2c
SomeKristi <kmaas2007@gmail.com>
2025-06-03 15:50:05 +0200

    fix for more nums

--- src/main/java/com/example/novakalkulacka/Kalkulacka.java
+++ src/main/java/com/example/novakalkulacka/Kalkulacka.java
@@ -10,33 +10,51 @@ public class Kalkulacka {
 
         boolean isNumeric = true;
         for (char c: arg.toCharArray()) {
-            if (c >= '0' && c <= '9') {
+            if (c >= '0' && c <= '9' || c == '.') {
                 isNumeric = true;
             } else {
                 isNumeric = false;
             }
 
-            if ((c == '+' || c == '-' || c == '*' || c == '/') && state.equals("num1")) {
-                operation = c;
-                state = "num2";
+            if (c == '+' || c == '-' || c == '*' || c == '/') {
+
+                if (state.equals("num")) {
+                    if (operation == '+')
+                        num1 = String.valueOf(Float.parseFloat(num1) + Float.parseFloat(num2));
+                    if (operation == '-')
+                        num1 = String.valueOf(Float.parseFloat(num1) - Float.parseFloat(num2));
+                    if (operation == '*')
+                        num1 = String.valueOf(Float.parseFloat(num1) * Float.parseFloat(num2));
+                    if (operation == '/')
+                        num1 = String.valueOf(Float.parseFloat(num1) / Float.parseFloat(num2));
+
+                    num2 = "";
+                    operation = c;
+                }
+                if (state.equals("num1")) {
+                    operation = c;
+                    state = "num";
+                }
             }
 
             if (state.equals("num1") && isNumeric) {
                 num1 += c;
-            } else if (state.equals("num2") && isNumeric) {
+            } else if (state.equals("num") && isNumeric) {
                 num2 += c;
             }
+
         }
 
         if (operation == '+')
-            return Float.parseFloat(num1) + Float.parseFloat(num2);
+            num1 = String.valueOf(Float.parseFloat(num1) + Float.parseFloat(num2));
         if (operation == '-')
-            return Float.parseFloat(num1) - Float.parseFloat(num2);
+            num1 = String.valueOf(Float.parseFloat(num1) - Float.parseFloat(num2));
         if (operation == '*')
-            return Float.parseFloat(num1) * Float.parseFloat(num2);
+            num1 = String.valueOf(Float.parseFloat(num1) * Float.parseFloat(num2));
         if (operation == '/')
-            return Float.parseFloat(num1) / Float.parseFloat(num2);
-        return 0;
+            num1 = String.valueOf(Float.parseFloat(num1) / Float.parseFloat(num2));
+
+        return Float.parseFloat(num1);
     }
 
 
